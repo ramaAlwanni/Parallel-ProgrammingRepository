1a0ee5fab1309e320954147106ab0e03e7666324
Yasmin Ebrahim(Y Ebrahim) <yasmineebrahim458@gmail.com>
2026-04-29 21:09:52 +0300

    Define fillable properties in order model
    Added fillable properties for user_id and product_id.

--- app/Models/order.php
+++ app/Models/order.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class order extends Model
 {
-    //
+   protected $fillable = [
+    'user_id',
+    'product_id',
+
+   ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
